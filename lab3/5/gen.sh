5fe49ebfa0c29ef12a0d811b29f8489b1abfba9e
Natalya Yultsova <60837582+yults@users.noreply.github.com>
2020-11-09 18:17:34 +0300

    lab3

--- lab3/5/gen.sh
+++ lab3/5/gen.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-while [[ true ]]
-do 
+while true; do 
 read s
 echo $s > pipe
 if [[ $s == "QUIT" ]]
 then
 exit 0
 fi
-if [[ $s= != "+" && $s != "*" && ! $s =~ [0-9]+ ]]
+if [[ $s= != "+" && $s != "M" && ! $s =~ [0-9]+ ]]
 then
 exit 1
 fi
