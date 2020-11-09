1bc88e8efd941d3bb61d8a3d272d28cd81fd50b1
Natalya Yultsova <60837582+yults@users.noreply.github.com>
2020-11-09 17:09:23 +0300

    changed

--- lab2/1.sh
+++ lab2/1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ps a u x | awk '$1=="yults"' | wc -l > 1.txt
-ps a u x | awk '$1=="yults" {print $2":"$11}' >> 1.txt
+ps a u x > save.txt
+cat save.txt | awk '$1=="yults"' | wc -l > 1.txt
+cat save.txt | awk '$1=="yults" {print $2":"$11}' >> 1.txt
